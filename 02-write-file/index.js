17111b6ad11000dc9f28dc44b21a3f184520375e
Александр Егоров <a.egorov@ndv.ru>
2022-05-21 18:17:52 +0300

    refactor: change 02 task logic

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,35 +8,39 @@ const nameFile = 'text.txt'
 const writePath = path.join(__dirname, nameFile);
 
 const input = process.stdin;
-const output = process.stdout
+const output = process.stdout;
+
+let message = ''
 
 const rl = readline.createInterface({input, output});
 
 console.log('Приветствую!');
-
-fs.writeFile(writePath, '', (error) => {
-    if(error) throw error;
-})
 
 process.on('exit', (code) => {
     console.log('\nВыходим, до свидания.');
 })
 
 
-rl.question('Тут что-то можно написать: ', (answer) => {});
+rl.question('Тут что-то можно написать: ', (answer) => {
+    message = answer;
+});
 
 rl.on('line', (input) => {
-    const write = `${input}\n`
+    const write = `\n${input}`
+
     if (input === 'exit') {
         rl.close();
     } else {
-        fs.appendFile(writePath, write, (error) => {
-            if(error) throw error;
-        })
+        message += write;
     }
 });
 
 rl.on('close', () => {
     console.log(`\nВсе написанное записано в файл ${nameFile}`);
+
+    fs.writeFile(writePath, message, (error) => {
+        if(error) throw error;
+    })
+
     rl.close();
 })
