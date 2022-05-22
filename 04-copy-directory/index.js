9a26e997d5cf2715ac9c789dccfbc95dc44c952a
Александр Егоров <a.egorov@ndv.ru>
2022-05-22 17:31:15 +0300

    refactor: 04 task add create and copy method

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,38 +9,35 @@ const newFolderPath = path.join(__dirname, newFolderName);
 
 const originalFolder = fs.promises.readdir(folderPath);
 
+/**
+ * Создание папки и копирывание туда файлов
+ * @param {string[]} files - массв имен файлов
+ */
+const createAndCopy = (files) => {
+    const createFolder = fs.promises.mkdir(newFolderPath);
+    createFolder
+        .then(() => {
+            for (let file of files) {
+                fs.copyFile(path.join(folderPath, file), path.join(newFolderPath, file), (error) =>{
+                    if (error) throw error;
+                })
+            }
+        })
+        .catch((error) => {
+            if (error) throw error;
+        })
+}
+
 originalFolder
     .then((files) => {
         fs.stat(newFolderPath, (error) => {
             if (error && error.code === 'ENOENT') {
-                const createFolder = fs.promises.mkdir(newFolderPath);
-                createFolder
-                    .then(() => {
-                        for (let file of files) {
-                            fs.copyFile(path.join(folderPath, file), path.join(newFolderPath, file), (error) =>{
-                                if (error) throw error;
-                            })
-                        }
-                    })
-                    .catch((error) => {
-                        if (error) throw error;
-                    })
+                createAndCopy(files);
             } else {
                 const removeFolder = fs.promises.rm(newFolderPath, {recursive: true, force: true});
                 removeFolder
                     .then(() => {
-                        const createFolder = fs.promises.mkdir(newFolderPath);
-                        createFolder
-                            .then(() => {
-                                for (let file of files) {
-                                    fs.copyFile(path.join(folderPath, file), path.join(newFolderPath, file), (error) =>{
-                                        if (error) throw error;
-                                    })
-                                }
-                            })
-                            .catch((error) => {
-                                if (error) throw error;
-                            })
+                        createAndCopy(files);
                     })
                     .catch((error) => {
                         if (error) throw error;
